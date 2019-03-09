d3bf29eac04e8cdbb77061963085b95e951f77f5
plzandtanku <mrkennethliu@gmail.com>
2019-03-09 10:20:54 -0800

    WIP

---
+++ htshark.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+	echo "I NEED A FILE PLZ"
+	exit
+fi
+
+echo "Processing: $1 ..."
+for entry in $1/*
+do
+	my_arr=$(echo $entry | tr "/" "\n")
+	for e in $my_arr
+	do
+		last=$e
+	done
+	/c/Program\ Files/Wireshark/tshark.exe -Y udp -nr $entry -T fields -e frame.len | sort -n | uniq -c > distributions/$last.txt
+
+	echo $last
+
+
+done
+
+#/c/Program\ Files/Wireshark/tshark.exe -Y udp -nr $1 -T fields -e frame.len | sort -n | uniq -c > dump.txt
+#
+#if [ ! -z "$2" ]
+#then
+#	/c/Program\ Files/Wireshark/tshark.exe -Y udp -nr $1 -T fields -e frame.len | sort -n | uniq -c > $2
+#fi
+#
+#
+#
+#echo "Dropped contents into dump.txt"
+#python kowalski.py
--- kowalski.py
+++ kowalski.py
@@ -1,21 +1,71 @@
+import os
 import sys
-import subprocess
 
-#if (len(sys.argv) < 2):
+myDir = None
+if (len(sys.argv) > 1):
+    myDir = sys.argv[1]
+   #if (len(sys.argv) < 2):
 #    print("ya burnt")
 #    sys.exit(1)
 #
 #fileName = sys.argv[1]
 #dumpFile = "~/Documents/school/cs239/dump.txt"
-dumpFile = "dump.txt"
+d = "dump.txt"
 
 
 #cmdstring = "tshark -Y udp -nr " + fileName + " -T fields -e frame.len | sort -n | uniq -c > " + dumpFile
+def analyzeFile(dumpFile):
+    f=open(dumpFile, "r")
+    if f.mode == 'r':
+        lines = f.read().splitlines()
+        first = lines[0].strip().split(" ")
+        last = lines[len(lines) - 1].strip().split(" ")
+        print("Min="+first[1]+", Count=" + first[0])
+        print("Max="+last[1]+", Count=" + last[0])
+        total = 0
+        for line in lines:
+            count = line.strip().split(" ")[0]
+            total += int(count)
+        print ("Num Packets=" + str(total))
 
-f=open(dumpFile, "r")
-if f.mode == 'r':
-    lines = f.read().splitlines()
-    first = lines[0].strip().split(" ")
-    last = lines[len(lines) - 1].strip().split(" ")
-    print("Min="+first[1]+", Count=" + first[0])
-    print("Max="+last[1]+", Count=" + last[0])
+
+def readConv(fileName):
+    f=open(fileName, "r")
+    if f.mode == 'r':
+        lines = f.read().splitlines()
+        for line in lines:
+            arr = line.split()
+            print(arr)
+readConv(d)
+sys.exit(1)
+if (myDir is None):
+    print("analyzing single file")
+    analyzeFile(d)
+else:
+    dic={"maxSize":0, "fileName":""}
+    dic2={"mostP":0,"fileName":""}
+    dic3={"minSize":-1,"fileName":""}
+    for f in os.listdir(myDir):
+        if (len(f.split(".")) > 1 and f.split(".")[len(f.split("."))-1] == 'txt'):
+            fil=open(myDir+f, "r")
+            if fil.mode == 'r':
+                lines = fil.read().splitlines()
+                first = lines[0].strip().split(" ")
+                last = lines[len(lines) - 1].strip().split(" ")
+                if (int(first[1]) < dic3["minSize"] or dic3["minSize"] == -1):
+                    dic3["minSize"] = int(first[1])
+                    dic3["fileName"] = f
+                if (int(last[1]) > dic["maxSize"]):
+                    dic["maxSize"] = int(last[1])
+                    dic["fileName"] = f
+
+                total = 0
+                for line in lines:
+                    count = line.strip().split(" ")[0]
+                    total += int(count)
+                if (total > dic2["mostP"]):
+                    dic2["mostP"] = total
+                    dic2["fileName"] = f
+    print(dic) 
+    print(dic2)
+    print(dic3)
